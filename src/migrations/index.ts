d9f6c0232d699131672fef9d0556cec616b5a6d2
Chris Fowles <chris@niftic.agency>
2026-01-08 11:30:23 -0700

    fix: nuclear option

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -3,7 +3,7 @@ import * as migration_20251210_164823 from './20251210_164823';
 import * as migration_20251218_180743 from './20251218_180743';
 import * as migration_20251218_220204 from './20251218_220204';
 import * as migration_20260107_160603 from './20260107_160603';
-import * as migration_20260108_161752 from './20260108_161752';
+import * as migration_20260108_182945 from './20260108_182945';
 
 export const migrations = [
   {
@@ -32,8 +32,8 @@ export const migrations = [
     name: '20260107_160603',
   },
   {
-    up: migration_20260108_161752.up,
-    down: migration_20260108_161752.down,
-    name: '20260108_161752'
+    up: migration_20260108_182945.up,
+    down: migration_20260108_182945.down,
+    name: '20260108_182945'
   },
 ];
