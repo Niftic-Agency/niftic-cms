165c9854a41e4c5e21b928d4559afc254735e9f6
tom <tom@niftic.agency>
2026-01-08 16:41:14 -0500

    Migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,8 +1,9 @@
-import * as migration_20250929_111647 from './20250929_111647'
-import * as migration_20251210_164823 from './20251210_164823'
-import * as migration_20251218_180743 from './20251218_180743'
-import * as migration_20251218_220204 from './20251218_220204'
-import * as migration_20260107_160603 from './20260107_160603'
+import * as migration_20250929_111647 from './20250929_111647';
+import * as migration_20251210_164823 from './20251210_164823';
+import * as migration_20251218_180743 from './20251218_180743';
+import * as migration_20251218_220204 from './20251218_220204';
+import * as migration_20260107_160603 from './20260107_160603';
+import * as migration_20260108_214112 from './20260108_214112';
 
 export const migrations = [
   {
@@ -30,4 +31,9 @@ export const migrations = [
     down: migration_20260107_160603.down,
     name: '20260107_160603',
   },
-]
+  {
+    up: migration_20260108_214112.up,
+    down: migration_20260108_214112.down,
+    name: '20260108_214112'
+  },
+];
